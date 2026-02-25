5ee7f57266a890323977f0d2efd0c09bb998d5da
Eugene Gincu <eugene.gincu@gmail.com>
2026-02-24 23:48:02 -0700

    Chapter 3 Select query clause

--- Chapter3.sql
+++ Chapter3.sql
@@ -1,2 +1,11 @@
 USE sakila;
 SHOW TABLES;
+SELECT *
+    FROM actor;
+SELECT actor_id,
+       'PENELOPE' name,
+       LOWER(last_name) last_name
+    FROM actor;
+SELECT version() version,
+       user() user,
+       database() database_name;
