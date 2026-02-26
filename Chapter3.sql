db530e4359b334b261ebb31bbaad1ca351461bc9
Eugene Gincu <eugene.gincu@gmail.com>
2026-02-25 23:43:15 -0700

    Chapter 3 From query clause

--- Chapter3.sql
+++ Chapter3.sql
@@ -4,8 +4,20 @@ SELECT *
     FROM actor;
 SELECT actor_id,
        'PENELOPE' name,
-       LOWER(last_name) last_name
+       LOWER(last_name) AS last_name  #optional AS keyword column alias
     FROM actor;
 SELECT version() version,
        user() user,
        database() database_name;
+SELECT DISTINCT actor_id   #distinct set removes duplicates
+    FROM film_actor
+    ORDER BY actor_id;
+SELECT ALL actor_id   #ALL is default
+    FROM film_actor
+    ORDER BY actor_id;
+SELECT concat(cust.last_name, ', ', cust.first_name) full_name
+    FROM
+        (SELECT first_name, last_name, email        #subquery (parentheses) in FROM is derived table
+         FROM customer
+         WHERE first_name = 'JESSIE'
+         ) cust;
