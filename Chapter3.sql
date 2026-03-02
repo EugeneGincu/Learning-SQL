b8503ab4ccbb8bb8dbeaddf35b4a88b1a693b725
Eugene Gincu <eugene.gincu@gmail.com>
2026-03-01 23:55:09 -0700

    Chapter 3 WHERE query clause, GROUP BY and HAVING clauses, ORDER BY clause

--- Chapter3.sql
+++ Chapter3.sql
@@ -49,3 +49,55 @@ SELECT c.first_name, c.last_name,
         INNER JOIN rental AS r      #optional AS for table alias
         ON c.customer_id = r.customer_id
     WHERE date(r.rental_date) = '2005-06-14';
+SELECT title
+    FROM film
+    WHERE rating = 'G' AND rental_duration >= 7;
+SELECT title
+    FROM film
+    WHERE rating = 'G' OR rental_duration >= 7;
+SELECT title, rating, rental_duration
+    FROM film
+    WHERE (rating = 'G' AND rental_duration >= 7)
+        OR (rating = 'PG-13' AND rental_duration < 4);
+SELECT c.first_name, c.last_name, count(*)
+    FROM customer c
+        INNER JOIN rental r
+        ON c.customer_id = r.customer_id
+    GROUP BY c.first_name, c.last_name
+    HAVING count(*) >= 40;
+SELECT c.first_name, c.last_name,
+        time(r.rental_date) rental_time
+    FROM customer c
+        INNER JOIN rental r
+        ON c.customer_id = r.customer_id
+    WHERE date(r.rental_date) = '2005-06-14'
+    ORDER BY c.last_name;
+SELECT c.first_name, c.last_name,
+        time(r.rental_date) rental_time
+    FROM customer c
+        INNER JOIN rental r
+        ON c.customer_id = r.customer_id
+    WHERE date(r.rental_date) = '2005-06-14'
+    ORDER BY c.last_name, c.first_name;
+SELECT c.first_name, c.last_name,
+        time(r.rental_date) rental_time
+    FROM customer c
+        INNER JOIN rental r
+        ON c.customer_id = r.customer_id
+    WHERE date(r.rental_date) = '2005-06-14'
+    ORDER BY time(r.rental_date) desc;
+SELECT c.first_name, c.last_name,
+        time(r.rental_date) rental_time
+    FROM customer c
+        INNER JOIN rental r
+        ON c.customer_id = r.customer_id
+    WHERE date(r.rental_date) = '2005-06-14'
+    ORDER BY time(r.rental_date) desc
+    LIMIT 5;
+SELECT c.first_name, c.last_name,
+        time(r.rental_date) rental_time
+    FROM customer c
+        INNER JOIN rental r
+        ON c.customer_id = r.customer_id
+    WHERE date(r.rental_date) = '2005-06-14'
+    ORDER BY 3 desc;
